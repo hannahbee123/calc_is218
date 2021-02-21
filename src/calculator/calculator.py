6ff0cebbe79c463e2720b5c814236e657c32477a
hbenjamin529 <31874276+hbenjamin529@users.noreply.github.com>
2021-02-21 18:28:19 -0500

    Feature: added multiplication method and unit test

--- src/calculator/calculator.py
+++ src/calculator/calculator.py
@@ -1,5 +1,6 @@
 from src.calculator.addition import addition
 from src.calculator.subtraction import subtraction
+from src.calculator.multiplication import multiplication
 
 
 class Calculator:
@@ -14,4 +15,8 @@ class Calculator:
 
     def subtract(self, a, b):
         self.result = subtraction(a, b)
+        return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
         return self.result
--- tests/calculatorTests.py
+++ tests/calculatorTests.py
@@ -23,6 +23,11 @@ class CalculatorTestCase(unittest.TestCase):
         self.assertEqual(calculator.subtract(2, 2), 0)
         self.assertEqual(calculator.result, 0)
 
+    def test_multiply_method_calculator(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.multiply(2, 2), 4)
+        self.assertEqual(calculator.result, 4)
+
 
 if __name__ == "__main__":
     unittest.main()
